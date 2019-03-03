33ae83b03d4787e45746173c1c407d7701a0a6e4
C0mpy <n.zunic@yahoo.com>
2019-03-03 16:00:46 +0100

    Added logging to user-result controller

--- app/controllers/user_results_controller.rb
+++ app/controllers/user_results_controller.rb
@@ -7,7 +7,9 @@ class UserResultsController < ApplicationController
 	skip_before_action :verify_authenticity_token
 	def create
   
-    puts params.inspect
+		puts params.inspect
+		puts "Saving user_result..."
+		logger.fatal "Saving user_result..."
 		survey_result = SurveyResult.find(params[:survey_result_id])
 
 		period_start = Time.at(params[:period_start].to_i/1000)
@@ -17,6 +19,8 @@ class UserResultsController < ApplicationController
 			time_spent_on_phone: params[:time_spent_on_phone], period_start: period_start, 
 			period_end: period_end)
 		user_result.save
+		puts "Saving user_result success..."
+		logger.fatal "Saving user_result success..."
 		render status: 200, json: @controller.to_json
 	end
 	
