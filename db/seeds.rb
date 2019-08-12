286418807bdbcbd92e8608e117580715b303f5ec
C0mpy <n.zunic@yahoo.com>
2019-08-13 00:08:25 +0200

    changing survey dates so that we dont have surveys that last forever

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 survey1 = Survey.create!(
     title: "This is the inital Survey deployed to mobile devices.", 
     description: "Please answer questions on a scale 1 - 5", 
-    start_time: Date.new(1, 1, 1), 
-    end_time: Date.new(3000, 1, 1))
+    start_time: Date.new(2019, 8, 10), 
+    end_time: Date.new(2019, 8, 25))
     
 Question.create(content: "How would you rate your quality of sleep?", survey: survey1)
 Question.create(content: "How would you rate your attention span?", survey: survey1)
@@ -18,7 +18,9 @@ Question.create(content: "How would you rate your attention span?", survey: surv
 survey2 = Survey.create(
     title: "Survey from rails server.", 
     description: "This is your initial survey. Please answer the questions listed on a scale of 1 - 5.",
-    start_time: Date.today)
+    start_time: Date.new(2019, 10, 10),
+    end_time: Date.new(2019, 12, 15)
+)
 
 Question.create(content: "How would you rate your ability to multitask?", survey: survey2)
 Question.create(content: "Would you describe yourself as a tech addict?", survey: survey2)
@@ -26,8 +28,8 @@ Question.create(content: "Would you describe yourself as a tech addict?", survey
 survey3 = Survey.create(
     title: "Future Survey, still in the making", 
     description: "This survey is not finished yet...",
-    start_time: 1.year.from_now, 
-    end_time: 2.year.from_now)
+    start_time: Date.new(2019, 12, 30), 
+    end_time: Date.new(2020, 2, 10))
 
 Question.create(content: "Would you describe yourself as forgetful?", survey: survey3)
 Question.create(content: "How would you rate your face-to-face interactions?", survey: survey3)
