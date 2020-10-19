808b2fb1f640d5857d34c0a6f22b5bbff2301914
Nemanja Zunic <nzunic.dev@gmail.com>
2020-10-19 13:25:51 +0200

    updating dates for the data seeded into DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ survey1 = Survey.create!(
     title: "This is the inital Survey deployed to mobile devices.", 
     description: "Please answer questions on a scale 1 - 5", 
     start_time: Date.new(2019, 8, 10), 
-    end_time: Date.new(2025, 8, 25))
+    end_time: Date.new(2021, 1, 31))
     
 Question.create(content: "How would you rate your quality of sleep?", survey: survey1)
 Question.create(content: "How would you rate your attention span?", survey: survey1)
@@ -18,8 +18,8 @@ Question.create(content: "How would you rate your attention span?", survey: surv
 survey2 = Survey.create(
     title: "Survey from rails server.", 
     description: "This is your initial survey. Please answer the questions listed on a scale of 1 - 5.",
-    start_time: Date.new(2019, 10, 10),
-    end_time: Date.new(2025, 12, 15)
+    start_time: Date.new(2021, 1, 1),
+    end_time: Date.new(2021, 3, 1)
 )
 
 Question.create(content: "How would you rate your ability to multitask?", survey: survey2)
@@ -28,8 +28,8 @@ Question.create(content: "Would you describe yourself as a tech addict?", survey
 survey3 = Survey.create(
     title: "Future Survey, still in the making", 
     description: "This survey is not finished yet...",
-    start_time: Date.new(2025, 12, 30), 
-    end_time: Date.new(2030, 2, 10))
+    start_time: Date.new(2021, 2, 1), 
+    end_time: Date.new(2021, 3, 31))
 
 Question.create(content: "Would you describe yourself as forgetful?", survey: survey3)
 Question.create(content: "How would you rate your face-to-face interactions?", survey: survey3)
